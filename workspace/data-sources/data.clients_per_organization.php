73188a7de1136acdd104f1e55f76bb775695354f
Jesse Sutherland <jesse@morsekode.com>
2012-06-07 12:58:07 -0500

    Tim's changes to make filtering work correctly

--- workspace/data-sources/data.clients_per_organization.php
+++ workspace/data-sources/data.clients_per_organization.php
@@ -4,7 +4,7 @@
 
     Class datasourceClients_per_organization extends Datasource{
 
-        function about(){
+        public function about(){
             return array(
                     'name' => 'Clients Per Organization',
                     'author' => array(
@@ -14,13 +14,26 @@
                     'version' => '1.0',
                     'release-date' => '2008-12-09T12:00:00+00:00');
         }
+        
+        public function __construct(&$parent, $env=NULL, $process_params=true){
+			parent::__construct($parent, $env, $process_params);
+			$this->_dependencies = array('$ds-logged-in-member');
+		}
 
         function grab(&$param_pool){
 
-            $client_list_id = "33";
             $result = new XMLElement("clients-per-organization");
+            
+            $params = $this->_env['env']['pool'];
+            $project = ($params['ds-logged-in-member'][0]);
 
-            $clients = $this->_Parent->Database->fetch("SELECT DISTINCT(handle), COUNT(handle) AS count, value FROM sym_entries_data_$client_list_id GROUP BY handle ORDER BY handle ASC");
+            
+            $clients = $this->_Parent->Database->fetch("SELECT sym_entries_data_33.*, COUNT(sym_entries_data_33.handle) AS count, value  
+				FROM sym_entries_data_30
+				RIGHT JOIN sym_entries_data_33
+				ON sym_entries_data_30.entry_id = sym_entries_data_33.entry_id
+				WHERE sym_entries_data_30.relation_id = $project GROUP BY handle ORDER BY handle ASC"
+			);
 
             foreach($clients as $client){
                 $client_node = new XMLElement("client", General::sanitize($client["value"]));
--- workspace/data-sources/data.tags_per_project.php
+++ workspace/data-sources/data.tags_per_project.php
@@ -3,8 +3,8 @@
     require_once(TOOLKIT . '/class.datasource.php');
 
     Class datasourceTags_per_project extends Datasource{
-
-        function about(){
+	   
+       public function about(){
             return array(
                     'name' => 'Tags Per Project',
                     'author' => array(
@@ -14,21 +14,33 @@
                     'version' => '1.0',
                     'release-date' => '2008-12-09T12:00:00+00:00');
         }
+        
+		public function __construct(&$parent, $env=NULL, $process_params=true){
+			parent::__construct($parent, $env, $process_params);
+			$this->_dependencies = array('$ds-project-view-individual', '$ds-logged-in-member');
+		}
 
-        function grab(&$param_pool){
-
-            $tag_list_id = "14";
-            $result = new XMLElement("tags-per-project");
-
-            $tags = $this->_Parent->Database->fetch("SELECT DISTINCT(handle), COUNT(handle) AS count, value FROM sym_entries_data_$tag_list_id GROUP BY handle ORDER BY handle ASC");
-
-            foreach($tags as $tag){
+        public function grab(&$param_pool){
+            
+			$result = new XMLElement("tags-per-project");
+			      
+			$params = $this->_env['env']['pool'];
+			$project = ($params['ds-project-view-individual'][0]);
+			
+			$tags = $this->_Parent->Database->fetch("SELECT sym_entries_data_14.*, COUNT(sym_entries_data_14.handle) AS count, value  
+				FROM sym_entries_data_17
+				RIGHT JOIN sym_entries_data_14
+				ON sym_entries_data_17.entry_id = sym_entries_data_14.entry_id
+				WHERE sym_entries_data_17.relation_id = $project GROUP BY handle ORDER BY handle ASC"
+			);
+                      
+           foreach($tags as $tag){
                 $tag_node = new XMLElement("tag", General::sanitize($tag["value"]));
                 $tag_node->setAttributeArray(array("handle" => $tag["handle"], "count" => $tag["count"]));
                 $result->appendChild($tag_node);
-            }
-
-            return $result;
+           }
+	
+           return $result;
         }
     }
 
